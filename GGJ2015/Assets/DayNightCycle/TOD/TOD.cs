a37e0327c090327c0bfac5d814df738fdf2d1910
UpwindSpring01 <kevin_loddewykx@hotmail.be>
2015-01-24 09:17:55 +0100

    Day night cycle Event trigger for start night and day Trigger zones for shed and woods

---
+++ GGJ2015/Assets/DayNightCycle/TOD/TOD.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TOD : MonoBehaviour
+{
+    public float slider;
+    public float slider2;
+    public float Hour;
+    private float Tod;
+
+    public Light sun;
+
+    public int speed = 50;
+
+    public Color NightFogColor;
+    public Color DuskFogColor;
+    public Color MorningFogColor;
+    public Color MiddayFogColor;
+
+    public Color NightAmbientLight;
+    public Color DuskAmbientLight;
+    public Color MorningAmbientLight;
+    public Color MiddayAmbientLight;
+
+    public Color NightTint;
+    public Color DuskTint;
+    public Color MorningTint;
+    public Color MiddayTint;
+
+    public Material SkyBoxMaterial1;
+    public Material SkyBoxMaterial2;
+
+    public Color SunNight;
+    public Color SunDay;
+
+    public GameObject Water;
+    public bool IncludeWater = false;
+    public Color WaterNight;
+    public Color WaterDay;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed == 0)
+        {
+            return;
+        }
+        if (slider >= 1.0)
+        {
+            slider = 0;
+        }
+
+        Hour = slider * 24;
+        Tod = slider2 * 24;
+        sun.transform.localEulerAngles = new Vector3((slider * 360) - 90, 0, 0);
+        slider = slider + Time.deltaTime / speed;
+        sun.color = Color.Lerp(SunNight, SunDay, slider * 2);
+
+        if (IncludeWater == true)
+        {
+            Water.renderer.material.SetColor("_horizonColor", Color.Lerp(WaterNight, WaterDay, slider2 * 2 - 0.2f));
+        }
+
+        if (slider < 0.5)
+        {
+            slider2 = slider;
+        }
+        else if (slider > 0.5)
+        {
+            slider2 = (1 - slider);
+        }
+        sun.intensity = (slider2 - 0.2f) * 1.7f;
+
+
+        if (Tod < 4)
+        {
+            //it is Night
+            RenderSettings.skybox = SkyBoxMaterial1;
+            RenderSettings.skybox.SetFloat("_Blend", 0);
+            SkyBoxMaterial1.SetColor("_Tint", NightTint);
+            RenderSettings.ambientLight = NightAmbientLight;
+            RenderSettings.fogColor = NightFogColor;
+        }
+        else if (Tod > 4 && Tod < 6)
+        {
+            //it is Dusk
+            RenderSettings.skybox = SkyBoxMaterial1;
+            RenderSettings.skybox.SetFloat("_Blend", 0);
+            RenderSettings.skybox.SetFloat("_Blend", (Tod / 2) - 2);
+            SkyBoxMaterial1.SetColor("_Tint", Color.Lerp(NightTint, DuskTint, (Tod / 2) - 2));
+            RenderSettings.ambientLight = Color.Lerp(NightAmbientLight, DuskAmbientLight, (Tod / 2) - 2);
+            RenderSettings.fogColor = Color.Lerp(NightFogColor, DuskFogColor, (Tod / 2) - 2);
+        }
+        else if (Tod > 6 && Tod < 8)
+        {
+            //it is Morning
+            RenderSettings.skybox = SkyBoxMaterial2;
+            RenderSettings.skybox.SetFloat("_Blend", 0);
+            RenderSettings.skybox.SetFloat("_Blend", (Tod / 2) - 3);
+            SkyBoxMaterial2.SetColor("_Tint", Color.Lerp(DuskTint, MorningTint, (Tod / 2) - 3));
+            RenderSettings.ambientLight = Color.Lerp(DuskAmbientLight, MorningAmbientLight, (Tod / 2) - 3);
+            RenderSettings.fogColor = Color.Lerp(DuskFogColor, MorningFogColor, (Tod / 2) - 3);
+        }
+        else if (Tod > 8 && Tod < 10)
+        {
+            //it is getting Midday
+            RenderSettings.ambientLight = MiddayAmbientLight;
+            RenderSettings.skybox = SkyBoxMaterial2;
+            RenderSettings.skybox.SetFloat("_Blend", 1);
+            SkyBoxMaterial2.SetColor("_Tint", Color.Lerp(MorningTint, MiddayTint, (Tod / 2) - 4));
+            RenderSettings.ambientLight = Color.Lerp(MorningAmbientLight, MiddayAmbientLight, (Tod / 2) - 4);
+            RenderSettings.fogColor = Color.Lerp(MorningFogColor, MiddayFogColor, (Tod / 2) - 4);
+        }
+    }
+}
---
+++ GGJ2015/Assets/Project/Scripts/NightEvent.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NightEvent : MonoBehaviour
+{
+
+    public TOD tod;
+    public int hourToSleep = 18;
+    public int hourToGetUp = 8;
+    public int speedDay = 100;
+    public int speedNight = 20;
+
+    public Texture callToSleep;
+    public Texture nightEvent;
+
+    private List<string> callsToSleep = new List<string>();
+    private List<string> nightEvents = new List<string>();
+
+    private bool isSleeping = false;
+    private string text;
+
+    // Use this for initialization
+    void Start()
+    {
+        callsToSleep.Add("It's " + hourToSleep + " o'clock time for dinner.");
+        callsToSleep.Add("It's getting dark outside.");
+        callsToSleep.Add("Come inside it's getting cold.");
+
+        nightEvents.Add("Oh it's looks like it has rained.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = tod.Hour;
+        if (tod.speed != 0)
+        {
+            if (!isSleeping)
+            {
+                if (currentTime >= hourToSleep)
+                {
+                    tod.speed = 0;
+                    text = callsToSleep[Random.Range(0, callsToSleep.Count - 1)];
+                }
+            }
+            else
+            {
+                if (currentTime >= hourToGetUp &&  currentTime < hourToSleep)
+                {
+                    tod.speed = 0;
+                    text = nightEvents[Random.Range(0, nightEvents.Count - 1)];
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (tod.speed == 0)
+        {
+            if (!isSleeping)
+            {
+                GUI.Label(new Rect(800, 800, 300, 100), new GUIContent(text, callToSleep));
+                if (GUI.Button(new Rect(1100, 800, 100, 50), "Continue"))
+                {
+                    isSleeping = true;
+                    tod.speed = speedNight;
+                }
+            }
+            else
+            {
+                GUI.Label(new Rect(800, 800, 300, 100), new GUIContent(text, nightEvent));
+                if (GUI.Button(new Rect(1100, 800, 100, 50), "Continue"))
+                {
+                    isSleeping = false;
+                    tod.speed = speedDay;
+                }
+            }
+        }
+    }
+}
